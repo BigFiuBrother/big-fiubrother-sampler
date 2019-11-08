57bab78a1a70f3fa8211fb39cc440abf715a830c
Eduardo Neira <edun327@gmail.com>
2019-11-08 18:49:17 -0300

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
    name='big-fiubrother-sampler',
-   version='0.1.0',
+   version='0.2.0',
    description='Big Fiubrother Sampler application',
    license="GPLv3",
    long_description=long_description,
