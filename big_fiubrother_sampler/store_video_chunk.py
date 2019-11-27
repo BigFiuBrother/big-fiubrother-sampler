71341052e35f59a9809c7d1cef09dbd755885147
Eduardo Neira <edun327@gmail.com>
2019-11-26 23:19:08 -0300

    Fix to filename creation in store video chunk

--- big_fiubrother_sampler/store_video_chunk.py
+++ big_fiubrother_sampler/store_video_chunk.py
@@ -31,8 +31,8 @@ class StoreVideoChunk(QueueTask):
         id = self.db.add(video_chunk)
 
         filepath = path.join(
-            self.tmp_path, 
-            '{}.h264'.format(video_chunk.filename))
+            self.path, 
+            '{}.h264'.format(video_chunk.filename()))
         
         with open(filepath, 'wb') as file:
             file.write(message.payload)
